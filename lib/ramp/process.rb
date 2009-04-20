63491b84b4eeea4e1886841a6079f41ba629983a
WebGet <webget@webget.com>
2009-04-20 15:34:24 -0700

    process upgrades

--- lib/ramp/process.rb
+++ lib/ramp/process.rb
@@ -2,116 +2,120 @@
 #
 # ==Examples
 #
-#   puts Process.ps_text
-#   => print the raw ps text
+#   p = Process.ps
+#   puts p
+#   => the results of the 'ps' command for the current process id
 #
-#   hash = Process.ps_hash
-#   => each key is a ps field name, each value is the string value
+#   p = Process.ps(1234)
+#   puts p
+#   => the results of the 'ps' command for process id 1234
 #
-#   hash = Process.ps
-#   => same as ps_hash, with typecasting and aliases
-#
-#   Process.ps['pcpu']
-#   => percentage of cpu use, as a float
-#
-#   Process.ps['pmem']
-#   => percentage of memory use, as a float
+#   p = Process.pss
+#   p['%cpu'] => percentage of cpu use, as a float
+#   p['%mem'] => percentage of memory use, as a float
 ##
 
 module Process
 
-  PS_KEYS=%w'blocked bsdtime c caught class cp egid egroup eip esp etime euid euser f fgid fgroup fuid fuser group ignored label lwp ni nlwp nwchan pending pcpu pgid pid pmem ppid pri psr rgid rgroup rss rtprio ruid ruser s sched sgi_p sgid sgroup sid sig size stackp start_time stat suid suser sz time tname tpgid vsize wchan'
-
-  PS_COMMAND='ps h ww -o '+PS_KEYS.join(',')
-
-  PS_ALIASES=Hash[%w'
-  %cpu pcpu
-  %mem pmem
-  sig_block blocked
-  sigmask blocked
-  sig_catch caught
-  sigcatch caught
-  cls class
-  cls policy
-  cputime time
-  gid egid 
-  group egroup
-  uid euid 
-  uname euser
-  user euser 
-  flag f
-  flags f
-  fsuid fuid
-  sig_ignore ignored
-  sigignore ignored
-  spid lwp 
-  tid lwp 
-  nice ni
-  thcount nlwp
-  sig pending 
-  sig_pend pending
-  pgrp pgid 
-  rssize rss
-  rsz rss
-  state s
-  sess sid
-  session sid
-  svgid sgid
-  tt tname 
-  tty tname 
-  vsz vsize
-  ']
-
- # Return the 'ps' method parsed into integers, floats, etc., also including aliases.
- # OPTIMIZE: add dates, times
- def self.ps(pid=Process.pid)
-   ps=self.ps_hash(pid)
-   ps['c']      =ps['c'].to_i
-   ps['cp']     =ps['cp'].to_f
-   ps['egid']   =ps['egid'].to_i
-   ps['egroup'] =ps['egroup'].to_i
-   ps['uid']    =ps['uid'].to_i
-   ps['fgid']   =ps['fgid'].to_i
-   ps['lwp']    =ps['lwp'].to_i
-   ps['ni']     =ps['ni'].to_i
-   ps['nlwp']   =ps['nlwp'].to_i
-   ps['pcpu']   =ps['pcpu'].to_f
-   ps['pgid']   =ps['pgid'].to_i
-   ps['pid']    =ps['pid'].to_i
-   ps['pmem']   =ps['pmem'].to_f
-   ps['ppid']   =ps['ppid'].to_i
-   ps['rgid']   =ps['rgid'].to_i
-   ps['rss']    =ps['rss'].to_i
-   ps['ruid']   =ps['ruid'].to_i  
-   ps['sid']    =ps['sid'].to_i  
-   ps['sgid']   =ps['sgid'].to_i  
-   ps['suid']   =ps['suid'].to_i  
-   PS_ALIASES.each_pair{|k,v| ps[k]=ps[v]}
-  return ps
- end
-
-
- # Return the 'ps' command as a hash of column name keys and values.
- # This is particularly useful to skip the time cost of typecasting.
-
- def self.ps_hash(pid=Process.pid)
-   Hash[PS_KEYS.zip(ps_text(pid).split)]
- end
-
-
- # Return the 'ps' command in tab delimited format (tdf).
- # This is particularly useful for logging to a spreadsheet.
-
- def self.ps_tdf(pid=Process.pid)
-   self.ps_text.split.join("\t")
- end
-
 
  # Return the 'ps' command as one long text string.
- # This is particularly useful for logging to a text file.
+ # This is typically useful for logging to a text file.
 
- def self.ps_text(pid=Process.pid)
-   `#{PS_COMMAND} #{pid.to_i}`
+ def self.ps(pid=Process.pid)
+   `#{self.ps_command} #{pid.to_i}`
  end
+
+ # Return the 'ps' command as a hash of keys and values.
+ # OPTIMIZE: add dates, times
+ def self.pss(pid=Process.pid)
+   ps=self.ps(pid)
+   h=Hash[self.ps_keys.zip(ps.split)]
+   h['c']      =h['c'].to_i
+   h['cp']     =h['cp'].to_f
+   h['egid']   =h['egid'].to_i
+   h['egroup'] =h['egroup'].to_i
+   h['uid']    =h['uid'].to_i
+   h['fgid']   =h['fgid'].to_i
+   h['lwp']    =h['lwp'].to_i
+   h['ni']     =h['ni'].to_i
+   h['nlwp']   =h['nlwp'].to_i
+   h['pcpu']   =h['pcpu'].to_f
+   h['pgid']   =h['pgid'].to_i
+   h['pid']    =h['pid'].to_i
+   h['pmem']   =h['pmem'].to_f
+   h['ppid']   =h['ppid'].to_i
+   h['rgid']   =h['rgid'].to_i
+   h['rss']    =h['rss'].to_i
+   h['ruid']   =h['ruid'].to_i  
+   h['sid']    =h['sid'].to_i  
+   h['sgid']   =h['sgid'].to_i  
+   h['suid']   =h['suid'].to_i  
+   self.ps_aliases.each_pair{|k,v| h[k]=h[v]}
+  return h
+ end
+
+  def self.ps_aliases
+    @@ps_aliases||=Hash[%w'
+    %cpu pcpu
+    %mem pmem
+    sig_block blocked
+    sigmask blocked
+    sig_catch caught
+    sigcatch caught
+    cls class
+    cls policy
+    cputime time
+    gid egid 
+    group egroup
+    uid euid 
+    uname euser
+    user euser 
+    flag f
+    flags f
+    fsuid fuid
+    sig_ignore ignored
+    sigignore ignored
+    spid lwp 
+    tid lwp 
+    nice ni
+    thcount nlwp
+    sig pending 
+    sig_pend pending
+    pgrp pgid 
+    rssize rss
+    rsz rss
+    state s
+    sess sid
+    session sid
+    svgid sgid
+    tt tname 
+    tty tname 
+    vsz vsize
+  ']
+
+
+ def self.ps_aliases(=aliases)
+  @@ps_aliases=aliases
+ end
+
+
+ def self.ps_keys
+  @@ps_keys=%w'blocked bsdtime c caught class cp egid egroup eip esp etime euid euser f fgid fgroup fuid fuser group ignored label lwp ni nlwp nwchan pending pcpu pgid pid pmem ppid pri psr rgid rgroup rss rtprio ruid ruser s sched sgi_p sgid sgroup sid sig size stackp start_time stat suid suser sz time tname tpgid vsize wchan'
+
+
+ def self.ps_keys=(keys)
+  @@ps_keys=keys
+ end
+
+
+ def self.ps_command
+  @@ps_command||='ps h ww -o '+PS_KEYS.join(',')
+ end
+
+
+ def self.ps_command=(command)
+  @@ps_comannd=command
+ end
+
 
 end
