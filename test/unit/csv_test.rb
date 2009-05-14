317b0df2361494e265b88e4acde70713a6a70ff8
WebGet <webget@webget.com>
2009-05-14 15:57:48 -0700

    update

--- test/unit/csv_test.rb
+++ test/unit/csv_test.rb
@@ -4,13 +4,13 @@ require 'ramp'
 class CSVTest < Test::Unit::TestCase
 
  def test_http_headers
-   h=CSV::HTTP.headers
+   h=CSV.http_headers
    assert_equal(h["Content-Type"],'text/csv')
    assert_equal(h["Content-Disposition"],"attachment; filename=\"data.csv\"")
  end
 
  def test_http_headers_with_filename
-   h=CSV::HTTP.headers(filename=>'foo')
+   h=CSV.http_headers(filename=>'foo')
    assert_equal(h["Content-Type"],'text/csv')
    assert_equal(h["Content-Disposition"],"attachment; filename=\"foo\"")
  end
