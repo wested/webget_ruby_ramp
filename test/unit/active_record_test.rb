226be6a083adb26644bb7cb850c2662da35635ac
WebGet <webget@webget.com>
2009-06-09 15:07:57 -0700

    active_record#create_or_update_by

---
+++ test/unit/active_record_test.rb
@@ -0,0 +1,13 @@
+require 'test/unit'
+require 'ramp'
+require 'active_record'
+
+class ActiveRecordTest < Test::Unit::TestCase
+
+  def test_create_or_update
+  end
+
+  def test_create_or_update_by
+  end
+
+end
