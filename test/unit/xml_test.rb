9a5667123baef44d752708a5038278aa3fbeee69
WebGet <webget@webget.com>
2009-06-10 06:10:01 -0700

    xml

--- test/unit/xml_test.rb
+++ test/unit/xml_test.rb
@@ -4,8 +4,8 @@ require 'ramp'
 class XMLTest < Test::Unit::TestCase
 
  def test_load_dir_files
-  dirpath='test/unit/xml_test_*.yml'
-  expect=["test/unit/xml_test_1.yml","test/unit/xml_test_2.yml"]
+  dirpath='test/unit/xml_test_*.xml'
+  expect=["test/unit/xml_test_1.xml","test/unit/xml_test_2.xml"]
   actual=Dir[dirpath].sort
   assert_equal(expect,actual,"Dir[#{dirpath}] expects test data files")
  end
@@ -14,7 +14,7 @@ class XMLTest < Test::Unit::TestCase
   dirpath='test/unit/xml_test_*.xml'
   expect="abcdef"
   actual=''
-  XML.load_dir(dirpath){|doc| actual << doc.to_s }
+  XML.load_dir(dirpath){|doc| doc.elements.each('foo/bar'){|e| s+=e.attributes['x']}}
   assert_equal(expect,actual,'XML.load_dir')
  end
 
