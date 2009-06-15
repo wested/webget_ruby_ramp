b271424ae067802957374388fb2dce2546984f4e
WebGet <webget@webget.com>
2009-06-15 16:01:16 -0700

    update example

--- lib/ramp/integer.rb
+++ lib/ramp/integer.rb
@@ -3,12 +3,12 @@ class Integer
   # Syntactic sugar to yield n times to a block.
   # Return an array of any results.
   #
-  # Examples
-  #   3.maps{foo} => calls method foo 3 times, and returns an array of results
+  # ==Example
   #   3.maps{rand} => [0.0248131784304143, 0.814666170190905, 0.15812816258206]
   #
-  # Parallel to Integer#times
-  # That is, maps is similar to Integer#times except that the output from each
+  # ==Parallel to Integer#times
+  #
+  # Integer#maps is similar to Integer#times except that the output from each
   # call to the block is captured in an array element and that array is
   # returned to the calling code.
   
