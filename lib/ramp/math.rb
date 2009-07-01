ea8d140c501bb76f289b2fe2aefa28a61cf16d62
WebGet <webget@webget.com>
2009-07-01 11:30:30 -0700

    update

--- lib/ramp/math.rb
+++ lib/ramp/math.rb
@@ -1,16 +1,17 @@
 module Math
 
+  module_function
 
   # Returns the natural log of x 
 
-  def Math::ln(x)
+  def ln(x)
    Math.log(x)
   end
 
 
   # Returns the log of x in base b.
  
-  def Math::logn(x,b)
+  def logn(x,b)
     Math.ln(x)/Math.ln(b)
   end
 
