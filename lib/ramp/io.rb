923c223f93fa06bbaf15c991738157c436579f86
WebGet <webget@webget.com>
2009-10-08 20:21:23 -0700

    add IO#readrows options for row & col separators

--- lib/ramp/io.rb
+++ lib/ramp/io.rb
@@ -1,36 +1,63 @@
 class IO
 
+
   # Reads the entire file specified by name as individual lines as with IO#readlines,
   # and returns those lines in an array of rows, where each row is an array of fields.
   #
-  # - Lines are separated by _sep_string_, which defaults to $/ or "\n" 
-  # - Rows are split by _split_, which defaults to $; or /\t/
+  # ==Example
+  #   IO.readrows("my.tdf")
+  #    => [["A1","B1","C1"],["A2","B2","C2"],["A3","B3","C3"]]
+  #
+  # ==Options
+  # - Rows are separated by _row_ option, which defaults to Ruby's record separator $/ or "\n" 
+  # - Cols are separated by _col_ option, which defaults to Ruby's string split separator $; or "\t"
+  #
+  # ==Example with options suitable for a TDF file
+  #   IO.readrows("my.tdf", :row=>"\n", :col=>"\t") 
+  #
+  # Note: the col option is sent along to String#split, so can be a string or a regexp.
   #
   # See:
   # - File#readline
   # - File#readlines
   # - File#readrow
 
-  def IO.readrows(name, sep_string=$/, split=$;)
-    sep_string||="\n"
-    split||=/\t/
-    return IO.readlines(name, sep_string).map{|line| line.chomp(sep_string).split(split)}
+  def IO.readrows(name, options={})
+    row_sep||=options['row']||$/||"\n"
+    col_sep||=options['col']||$;||"\t"
+    return IO.readlines(name, row_sep).map{|line| line.chomp(row_sep).split(col_sep)}
   end
+
 
   # Read a line as with IO#readline and return the line as a row of fields.
   #
-  # - Lines are separated by _sep_string_, which defaults to $/ or "\n" 
-  # - Rows are split by _split_, which defaults to $; or /\t/
+  # ==Example
+  #   file = File.new("my.tdf")
+  #   file.readrow() => ["A1","B1","C1"]
+  #   file.readrow() => ["A2","B2","C2"]
+  #   file.readrow() => ["A3","B3","C3"]]
+  #
+  # ==Options
+  # - Rows are separated by _row_ option, which defaults to Ruby's record separator $/ or "\n" 
+  # - Cols are separated by _col_ option, which defaults to Ruby's string split separator $; or "\t"
+  #
+  # ==Example with options suitable for a TDF file
+  #   file = File.new("my.tdf")
+  #   file.readrow(:row=>"\n", :col=>"\t") => ["A1","B1","C1"] 
+  #   file.readrow(:row=>"\n", :col=>"\t") => ["A2","B2","C2"] 
+  #   file.readrow(:row=>"\n", :col=>"\t") => ["A3","B3","C3"] 
+  #
+  # Note: the col option is sent along to String#split, so can be a string or a regexp.
   #
   # See:
   # - File#readline
   # - File#readlines
   # - File#readrows
 
-  def readrow(sep_string=$/,split=$;)
-    sep_string||="\n"
-    split||=/\t/
-    return readline(sep_string).chomp(sep_string).split(split)
+  def readrow(options={})
+    row_sep||=options['row']||$/||"\n"
+    col_sep||=options['col']||$;||"\t"
+   return readline(row_sep).chomp(row_sep).split(col_sep)
   end
 
 end
--- test/unit/io_test.rb
+++ test/unit/io_test.rb
@@ -10,18 +10,18 @@ class IOTest < Test::Unit::TestCase
    assert_equal(expect,actual,"Dir[#{dirpath}] expects test data files")
  end
 
- def test_readline
+ def test_readrow
   filename=Dir['test/unit/io_test.txt'].first
   f=File.open(filename)
-  expect=["aa","bb","cc"]
-  actual=f.readrow("\n","-")
-  assert_equal(expect,actual)
+  assert_equal(["A1","B1","C1"],f.readrow(:row=>"=",:col=>"-"))
+  assert_equal(["A2","B2","C2"],f.readrow(:row=>"=",:col=>"-"))
+  assert_equal(["A3","B3","C3"],f.readrow(:row=>"=",:col=>"-"))
  end
 
- def test_readlines
+ def test_readrows
   filename=Dir['test/unit/io_test.txt'].first
-  expect=[["aa","bb","cc"],["dd","ee","ff"],["gg","hh","ii"]]
-  actual=IO.readrows(filename,"\n","-")
+  expect=[["A1","B1","C1"],["A2","B2","C2"],["A3","B3","C3"]]
+  actual=IO.readrows(filename,:row=>"=",:col=>"-")
   assert_equal(expect,actual)
  end
 
