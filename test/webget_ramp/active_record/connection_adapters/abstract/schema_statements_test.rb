7a2038c9c13c9b9bd46dd24afa3df404381f670e
WebGet <webget@webget.com>
2010-02-13 15:20:25 -0800

    updates for gemcutter

--- test/webget_ramp/active_record/connection_adapters/abstract/schema_statements_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test/unit'
-require 'webget_ramp'
-
-class SchemaStatementsTest < Test::Unit::TestCase
-
-  def test_placeholder
-  end
-
-end
--- test/webget_ramp/active_record/save_extensions_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test/unit'
-require 'webget_ramp'
-require 'active_record'
-
-class SaveExtensionsText < Test::Unit::TestCase
-end
-
