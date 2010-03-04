55e1bbb6786f36a43cd9d4d2916100fef11bc243
WebGet <webget@webget.com>
2010-03-03 18:52:38 -0800

    update test for ruby 187

--- test/webget_ruby_ramp/class_test.rb
+++ test/webget_ruby_ramp/class_test.rb
@@ -8,24 +8,33 @@ class ClassTest < Test::Unit::TestCase
   def test_publicize_methods
 
     # Before we test the block, ensure our setup is correct
-    assert_equal([:a,:a!,:a=,:a?],My.private_instance_methods.grep(METHOD_REGEXP),'private methods before block')
-    assert_equal([:b,:b!,:b=,:b?],My.protected_instance_methods.grep(METHOD_REGEXP),'protected methods before block')
-    assert_equal([:c,:c!,:c=,:c?],My.public_instance_methods.grep(METHOD_REGEXP),'public methods before block')
+    assert_equal(["a","a!","a=","a?"],My.private_instance_methods.grep_sort_to_s(METHOD_REGEXP),'private methods before block')
+    assert_equal(["b","b!","b=","b?"],My.protected_instance_methods.grep_sort_to_s(METHOD_REGEXP),'protected methods before block')
+    assert_equal(["c","c!","c=","c?"],My.public_instance_methods.grep_sort_to_s(METHOD_REGEXP),'public methods before block')
 
     # Now we test inside the block
     My.publicize_methods{
-      assert_equal([],My.private_instance_methods.grep(METHOD_REGEXP),'private methods inside block')
-      assert_equal([],My.protected_instance_methods.grep(METHOD_REGEXP),'protected methods inside block')
-      assert_equal([:a,:a!,:a=,:a?,:b,:b!,:b=,:b?,:c,:c!,:c=,:c?],My.public_instance_methods.grep(METHOD_REGEXP).sort,'public methods inside block')
+      assert_equal([],My.private_instance_methods.grep_sort_to_s(METHOD_REGEXP),'private methods inside block')
+      assert_equal([],My.protected_instance_methods.grep_sort_to_s(METHOD_REGEXP),'protected methods inside block')
+      assert_equal(["a","a!","a=","a?","b","b!","b=","b?","c","c!","c=","c?"],My.public_instance_methods.grep_sort_to_s(METHOD_REGEXP),'public methods inside block')
     }
 
     # After we test the block, ensure our setup is restored
-    assert_equal([:a,:a!,:a=,:a?],My.private_instance_methods.grep(METHOD_REGEXP),'private methods before block')
-    assert_equal([:b,:b!,:b=,:b?],My.protected_instance_methods.grep(METHOD_REGEXP),'protected methods before block')
-    assert_equal([:c,:c!,:c=,:c?],My.public_instance_methods.grep(METHOD_REGEXP),'public methods before block')
+    assert_equal(["a","a!","a=","a?"],My.private_instance_methods.grep_sort_to_s(METHOD_REGEXP),'private methods before block')
+    assert_equal(["b","b!","b=","b?"],My.protected_instance_methods.grep_sort_to_s(METHOD_REGEXP),'protected methods before block')
+    assert_equal(["c","c!","c=","c?"],My.public_instance_methods.grep_sort_to_s(METHOD_REGEXP),'public methods before block')
 
   end
 
+  protected
+
+end
+
+
+class Array
+  def grep_sort_to_s(regexp)
+    grep(regexp).sort.map{|x| x.to_s}
+  end
 end
 
 
